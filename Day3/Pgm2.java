749f2d06cc28af3f7a688294fb3f0e6af11b76c2
r-osejose <112931245+r-osejose@users.noreply.github.com>
2022-09-22 15:17:55 +0530

    Update Pgm2.java

--- Day3/Pgm2.java
+++ Day3/Pgm2.java
@@ -1,36 +1,50 @@
 package Day3;
-	class Member {
-		String name;
-		int age;
-		String phone;
-		String address;
-		int salary;
-		
-		public void printsalary()
-		{
-			System.out.println("Salary is : " +salary);
-		}
-
-class Employee extends Member{
-	String specialization;
+class member
+{
+    String name;
+    int age;
+    String phone;
+    String address;
+    int salary;
+    
+    public void printsalary()
+    {
+        System.out.println("Salary is:" +salary);
+    }
 }
 
-class Manager extends Member{
-	String department;
+class employee extends member
+{
+    String specialization;
+    
+}
+class manager extends member
+{
+    String department;
+    
 }
 
 public class Pgm2 {
+    public static void main(String args[])
+    {
+        employee emp = new employee();
+        emp.name= "Rose";
+        emp.age= 22;
+        emp.phone="7035718921";
+        emp.address="xyz";
+        emp.salary=23000;
+        emp.printsalary();
+        
+        manager mng = new manager();
+        mng.name= "Sona";
+        mng.age= 26;
+        mng.phone="8905346712";
+        mng.address="abc";
+        mng.salary=55000;
+        
+        
+    }
 
-	public static void main(String[] args) {
-		Employee emp = new Employee();
-		emp.name = "Chinnu";
-		emp.salary = 14710;
-		emp.printsalary();
-		
-		Manager mng = new Manager();
-		mng.salary = 22000;
-	}
 
-	}
 
 }
