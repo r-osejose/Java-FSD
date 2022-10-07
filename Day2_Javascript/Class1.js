0fccc8781429a8fff9f3ef4589671ed33050c85e
r-osejose <112931245+r-osejose@users.noreply.github.com>
2022-10-07 12:31:56 +0530

    Add files via upload

---
+++ Day2_Javascript/Class1.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const p1 = new Person('Rose');
+const p2 = new Person('Pink');
+console.log(p1.name);
+console.log(p2.name);
+
